92806850ae769de333c3f682f0f54873d9c943b6
benhimarthi <benhimarthi18@gmail.com>
2022-12-16 15:54:29 +0100

    commit_b

---
+++ src/main/java/org/sid/bilingservice/model/Customer.java
@@ -0,0 +1,10 @@
+package org.sid.bilingservice.model;
+
+import lombok.Data;
+
+@Data
+public class Customer {
+    private Long id;
+    private String name;
+    private String email;
+}
---
+++ src/main/java/org/sid/bilingservice/web/BillRestController.java
@@ -0,0 +1,33 @@
+package org.sid.bilingservice.web;
+
+import org.sid.bilingservice.entities.Bill;
+import org.sid.bilingservice.repository.BillRepository;
+import org.sid.bilingservice.repository.ProductItemRepository;
+import org.sid.bilingservice.service.CustomerRestClient;
+import org.sid.bilingservice.service.ProductRestClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BillRestController {
+    @Autowired
+    private BillRepository billRepository;
+    @Autowired
+    private ProductItemRepository productItemRepository;
+    @Autowired
+    private CustomerRestClient customerRestClient;
+    @Autowired
+    private ProductRestClient productRestClient;
+
+    @GetMapping( "/fullBill/{id}")
+    private Bill bill(@PathVariable Long id){
+        Bill bill = billRepository.findById(id).get();
+        bill.setCustomer(customerRestClient.findCustomerById(bill.getCustomerId()));
+        bill.getProductItems().forEach(pi->{
+            pi.setProduct(productRestClient.findProductById(pi.getProductId()));
+        });
+        return bill;
+    }
+}
